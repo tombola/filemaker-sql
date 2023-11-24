baa214a51dda399fa040a4c249d166f35187f056
tombola <tombola@github>
2023-11-24 07:39:39 +0000

    Test select statement

--- tests/connect_test.py
+++ tests/connect_test.py
@@ -1,4 +1,7 @@
 import pypyodbc
+from pypika import Query, Table, Field
+from rich import print
+import pypyodbc
 
 
 def test_always_true():
@@ -7,3 +10,15 @@ def test_always_true():
 
 def test_connect(fmdb):
     assert fmdb.cursor()
+
+
+def test_select(fmdb: pypyodbc.Connection):
+    cursor = fmdb.cursor()
+    q = Query.from_("Products").select("Date", "Part Number")
+    sql = q.get_sql()
+    print(sql)
+    results = cursor.execute(sql)
+    all_results = results.fetchall()
+
+    assert isinstance(results, pypyodbc.Cursor)
+    assert isinstance(all_results, list)
