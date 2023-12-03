b8aec191e54daa346b683dd94c290fb1484e7005
tombola <tombola@github>
2023-12-03 18:10:04 +0000

    Separate database settings with nested delimiter
    Only allows for a single database

--- src/fmsql/settings.py
+++ src/fmsql/settings.py
@@ -1,13 +1,20 @@
 from pydantic import Field
 
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from rich import print
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="fmsql_")
+class DatabaseSettings(BaseModel):
+    host: str = Field("localhost")
+    name: str = Field("")
+    user: str = Field("")
+    pwd: str = Field("")
 
-    database_host: str = Field("localhost", alias="host")
-    fmsql_database_name: str = Field("")
-    database_user: str = Field("")
-    database_pwd: str = Field("")
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="fmsql_",
+        env_nested_delimiter="__",
+    )
+    database: DatabaseSettings
